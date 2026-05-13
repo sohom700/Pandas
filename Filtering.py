c630fa75165bee9df1afb4dd0436de60c9619765
Sohom <ghoshsohom700@gmail.com>
2026-05-13 17:48:39 +0530

    Filtering Data

--- Filtering.py
+++ Filtering.py
@@ -1,5 +1,6 @@
 import pandas as pd
 df=pd.read_csv("Movies.csv")
+
 # runtime=df[df["Runtime (mins)"] > 130]
 # rating= df[df["IMDb Rating"] > 7.5]
 # director=df[df["Directors"] == "Harald Zwart"]
