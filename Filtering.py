e4d6e9e05e81a5638a121821f04c6e38dfff0496
Sohom <ghoshsohom700@gmail.com>
2026-05-13 17:48:09 +0530

    Filtering data

---
+++ Filtering.py
@@ -0,0 +1,10 @@
+import pandas as pd
+df=pd.read_csv("Movies.csv")
+# runtime=df[df["Runtime (mins)"] > 130]
+# rating= df[df["IMDb Rating"] > 7.5]
+# director=df[df["Directors"] == "Harald Zwart"]
+
+popularity=df[(df["IMDb Rating"]>7.5) |
+              (df["Runtime (mins)"]>130)]
+
+print(popularity[["Title","Runtime (mins)","Directors"]].to_string())
